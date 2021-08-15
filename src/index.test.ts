e32cde04bfc99e7da11fff265f5140dc96933296
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2021-08-15 16:51:20 -0400

    Rename delimiter => separator

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,17 +4,17 @@ describe('isSlugged', () => {
   it('returns true for a slugged string', () => {
     expect(isSlugged('a-slugged-string')).toStrictEqual(true);
   });
-  it('returns true for a non-delimited word if delimiters are not required', () => {
+  it('returns true for a word with no separators if separators are not required', () => {
     expect(isSlugged('abc')).toStrictEqual(true);
   });
-  it('returns false for a non-delimited word if at least one delimiter is required', () => {
-    expect(isSlugged('abc', { isDelimiterRequired: true })).toStrictEqual(false);
+  it('returns false for a word with no separators if at least one separator is required', () => {
+    expect(isSlugged('abc', { requireSeparator: true })).toStrictEqual(false);
   });
   it('returns false for an unslugged string', () => {
     expect(isSlugged('Full-time employees work full time. Real-time events happen in real time.')).toStrictEqual(false);
   });
-  it('supports custom delimiters', () => {
-    expect(isSlugged('a_slugged_string', { delimiter: '_' })).toStrictEqual(true);
-    expect(isSlugged('a-slugged-string', { delimiter: '_' })).toStrictEqual(false);
+  it('supports custom separators', () => {
+    expect(isSlugged('a_slugged_string', { separator: '_' })).toStrictEqual(true);
+    expect(isSlugged('a-slugged-string', { separator: '_' })).toStrictEqual(false);
   });
 });
